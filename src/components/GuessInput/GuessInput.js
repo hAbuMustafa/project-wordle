28417bb32dddfbbc2a73fe96a232bea609a46e1d
hosam5553 <hosam5553@gmail.com>
2024-02-06 18:36:17 +0000

    added skeleton for input

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React,{useState} from "react";
 
 function GuessInput() {
-  return <div></div>;
+  const [guess, setGuess] = useState("")
+  return (
+    <form class="guess-input-wrapper">
+      <label for="guess-input">Enter guess:</label>
+      <input id="guess-input" type="text" />
+    </form>
+  );
 }
 
 export default GuessInput;
