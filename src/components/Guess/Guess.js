86cbd7812247a775f0ac4e3bbce5db1cc31f8303
hosam5553 <hosam5553@gmail.com>
2024-02-06 20:32:53 +0000

    done with 3

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from "react";
+import { range } from "../../utils";
 
-function Guess() {
-  return <div></div>;
+function Guess({ guess }) {
+  return (
+    <p className="guess">
+      {range(5).map((_, i) => (
+        <span className="cell">{!!guess && guess.charAt(i)}</span>
+      ))}
+    </p>
+  );
 }
 
 export default Guess;
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,26 +1,13 @@
 import React from "react";
 import { range } from "../../utils";
+import Guess from "../Guess";
 
 function GuessResults({ guessList }) {
   return (
     <div className="guess-results">
-      {range(6).map((_, i) => {
-        if (guessList[i]) {
-          return (<p className="guess" key={i}>
-            {guessList[i].split("").map((letter) => (
-              <span className="cell">{letter}</span>
-            ))}
-          </p>)
-        } else {
-          return (
-            <p className="guess" key={i}>
-              {range(5).map(() => (
-                <span className="cell"></span>
-              ))}
-            </p>
-          );
-        }
-      })}
+      {range(6).map((_, i) => (
+        <Guess guess={guessList[i]} />
+      ))}
     </div>
   );
 }
