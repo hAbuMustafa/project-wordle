303926908fd45c4a15619c58c1a03784858ca450
hosam5553 <hosam5553@gmail.com>
2024-02-06 22:20:53 +0000

    Keyboard added

---
+++ src/components/Keyboard/Keyboard.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const rows = ["QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM"];
+
+function Keyboard() {
+  return (
+    <div className="keyboard">
+      {rows.map((row) => (
+        <p key={row}>
+          {row.split("").map((char) => (
+            <span>{char}</span>
+          ))}
+        </p>
+      ))}
+    </div>
+  );
+}
+
+export default Keyboard;
