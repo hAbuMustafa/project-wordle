9b64a3ad53f05853cea8c961f135ad44fddc9fa6
hosam5553 <hosam5553@gmail.com>
2024-02-06 19:38:52 +0000

    implemented min

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,11 +1,31 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 function GuessInput() {
-  const [guess, setGuess] = useState("")
+  const [guess, setGuess] = useState("");
   return (
-    <form class="guess-input-wrapper">
+    <form
+      class="guess-input-wrapper"
+      onSubmit={(e) => {
+        e.preventDefault();
+
+        console.log("Guess:", guess);
+
+        setGuess("");
+      }}
+    >
       <label for="guess-input">Enter guess:</label>
-      <input id="guess-input" type="text" />
+      <input
+        id="guess-input"
+        type="text"
+        required
+        pattern="[A-Z]{5}"
+        title="Enter a 5-letter word (real word 🙄)"
+        value={guess}
+        maxLength={5}
+        onChange={(e) => {
+          setGuess(e.target.value.toUpperCase());
+        }}
+      />
     </form>
   );
 }
